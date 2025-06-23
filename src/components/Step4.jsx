f4f6f002c4b3a90d865b242da54d8648f05dc038
mung-happy <mungnxhnv7@gmail.com>
2025-06-23 21:46:51 +0700

    update

--- src/components/Step4.jsx
+++ src/components/Step4.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Step4 = () => {
   return (
     <div className="center-text fade_out">
+      <img src="background.jpg" alt="" />
       <h1>Một ngày đặc biệt của một cô gái đặc biệt</h1>
     </div>
   );
